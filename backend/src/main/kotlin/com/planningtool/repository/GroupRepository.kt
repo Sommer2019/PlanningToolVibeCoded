8c80b5649bb68c4eff56ef427513e36366daa988
Aljan373 <aljan.ghawami@rfh-campus.de>
2026-06-17 09:57:36 +0200

    Delete backend directory

--- backend/src/main/kotlin/com/planningtool/repository/GroupRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.planningtool.repository
-
-import com.planningtool.model.Group
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface GroupRepository : JpaRepository<Group, Long>
